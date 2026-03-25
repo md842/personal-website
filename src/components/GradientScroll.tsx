9e9b8ccdf20123033806b22db93bd128b89bdfaa
Max Deng <deng.m.842@gmail.com>
2026-03-24 20:45:01 -0700

    Header mobile responsiveness improvements
    Header nav links now shrink into a `GradientScroll` container on phone screens. Brand is condensed on phone screens as it was taking up too much space anyway.
    
    Made some changes to `GradientScroll` to help its generalizability.
    
    There still isn't a lot of header content, so I like how this looks better than a collapsed hamburger menu, but that will have to be the approach if any more content is added to the header in the future.

--- src/components/GradientScroll.tsx
+++ src/components/GradientScroll.tsx
@@ -1,8 +1,8 @@
 import {type ReactNode} from "react";
 
-interface GradientScrollProps extends React.PropsWithChildren{
+interface GradientScrollProps extends React.PropsWithChildren, React.HTMLAttributes<HTMLDivElement>{
   // Width of the gradient on each side.
-  width: string; // Any valid CSS width string (e.g., rem, em, px, %, calc()).
+  gradWidth: string; // Any valid CSS width string (e.g., rem, em, px, %, calc()).
 
   // Color at the start of the gradient. Default: transparent
   startColor?: string; // Any valid CSS color string (e.g., hex, rgb(), rgba())
@@ -19,15 +19,19 @@ export default function GradientScroll(props: GradientScrollProps): ReactNode{
     (props.endColor ? props.endColor : "var(--bs-body-bg)") + " 100%)"
 
   return(
-    <div className="gradient-scroll position-relative">
+    <div style={props.style}
+      className={"gradient-scroll position-relative" + 
+        (props.className ? " " + props.className : "")
+      }
+    >
       <div className="gs-before position-absolute h-100"
-        style={{width: props.width,
+        style={{width: props.gradWidth,
           backgroundImage: "linear-gradient(to left, " + gradColors
         } as React.CSSProperties}
       />
       {props.children}
       <div className="gs-after position-absolute h-100 top-0 end-0"
-        style={{width: props.width, 
+        style={{width: props.gradWidth, 
           backgroundImage: "linear-gradient(to right, " + gradColors
         } as React.CSSProperties}
       />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,9 +5,11 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import GradientScroll from './GradientScroll';
 
 export default function Header(){
   const [darkMode, setDarkMode] = useState(initDarkMode);
+  const [brand, setBrand] = useState("Max Deng");
 
   function initDarkMode(): boolean{ // Dark mode state initializer function
     // Attempt to load saved dark mode setting from local storage
@@ -32,19 +34,47 @@ export default function Header(){
     localStorage.setItem('darkMode', darkMode.toString());
   }, [darkMode]); // Runs on mount and when darkMode changes
 
+  useEffect(() => { // Determine which brand to use on window resize
+    const onResize = () => { // Window resize event handler
+      setBrand(window.innerWidth >= 600 ? "Max Deng" : "MD");
+    }
+    window.addEventListener("resize", onResize);
+    onResize(); // Run once to set initial size
+    
+    return() => { // useEffect cleanup; removes event listener
+      window.removeEventListener("resize", onResize);
+    }
+  }, []); // Runs on mount
+
   return(
     <header className="sticky-top">
       <Navbar bg="secondary-subtle" data-bs-theme="dark">
         <Container>
-          <Nav
-            activeKey={'/' + useLocation().pathname.split('/')[1]}
-            variant="underline"
-          > {/* Highlights active page in nav bar */}
-            <Navbar.Brand as={Link} to="/">Max Deng</Navbar.Brand>
-            <Nav.Link as={Link} eventKey="/" to="/">Home</Nav.Link>
-            <Nav.Link as={Link} eventKey="/projects" to="/projects">Projects</Nav.Link>
-            <Nav.Link as={Link} eventKey="/leetcode" to="/leetcode">LeetCode</Nav.Link>
-          </Nav>
+          {/* Group Navbar Brand and Links away from dark mode button */}
+          <div className="d-flex flex-shrink-1" // Shrink when Navbar too small
+            style={{minWidth: 0}} // Avoid clamping to minimum size
+          >
+            <Navbar.Brand className="ms-3" as={Link} to="/">
+              {brand} {/* "Max Deng" or "MD" depending on screen size */}
+            </Navbar.Brand>
+
+            <GradientScroll className="mx-3 flex-shrink-1"
+              style={{minWidth: 0}} // Avoid clamping to minimum size
+              gradWidth="0.5rem"
+              endColor="var(--bs-secondary-bg-subtle)"
+            >
+              <Nav className="overflow-scroll" variant="underline"
+                // activeKey highlights active page in Navbar
+                activeKey={'/' + useLocation().pathname.split('/')[1]}
+                style={{scrollbarWidth: "none"}} // Hide scrollbar in Navbar
+              >
+                <Nav.Link as={Link} eventKey="/" to="/">Home</Nav.Link>
+                <Nav.Link as={Link} eventKey="/projects" to="/projects">Projects</Nav.Link>
+                <Nav.Link as={Link} eventKey="/leetcode" to="/leetcode">LeetCode</Nav.Link>
+              </Nav>
+            </GradientScroll>
+          </div>
+
           <Button variant="link"
             onClick={() => setDarkMode(prev => !prev)}
             style={{height: "3rem"}}
